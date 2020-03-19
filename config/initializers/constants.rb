7a4ec91b9f4c5c0dfa95f5452531f4f522f22d88
OrlandoLee <zongshengli@ucla.edu>
2020-03-18 20:44:59 -0700

    ol - add project types and filters

---
+++ config/initializers/constants.rb
@@ -0,0 +1,9 @@
+ALL_SKILLS = [ 'Software', 'Biology', 'Biotech', 'Medicine', 'Mechanics & Electronics', 'Funding', 'Content',  'Manufacturing', 'PM', 'Anything' ].freeze
+ALL_PROJECT_TYPES = [ 
+  'Track the outbreak', 
+  'Help out neighbours', 
+  'News and information', 
+  'Medical supplies', 
+  'Test the virus', 
+  'Other'
+  ].freeze
